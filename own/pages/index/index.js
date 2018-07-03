012090d3f553212bb90a268b98d79afbac945b32
bingtang <15580453953@163.com>
2018-07-03 17:04:29 +0800

    aa

---
+++ own/pages/index/index.js
@@ -0,0 +1,116 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+//index.js  
+Page({
+  data: {
+    movies: [
+      { url: 'http://img2.imgtn.bdimg.com/it/u=893396837,3696192511&fm=27&gp=0.jpg' },
+      { url: 'image/banner.jpg' },
+      { url: 'image/banner1.jpg' }
+    ],
+    TeaList: [
+      {
+        url: "image/img-1.gif",
+        title: "玉芝茉莉",
+        price: "￥16"
+      },
+      {
+        url: "image/img-2.gif",
+        title: "芝凤四季春",
+        price: "￥16"
+      },
+      {
+        url: "image/img-3.gif",
+        title: "芝凤鸳鸯",
+        price: "￥18"
+      },
+      {
+        url: "image/img-4.gif",
+        title: "芝凤红韵",
+        price: "￥18"
+      },
+      {
+        url: "image/img-5.gif",
+        title: "蜜桃成熟时",
+        price: "￥20"
+      },
+      {
+        url: "image/img-6.gif",
+        title: "蜜桃番石榴绿研",
+        price: "￥20"
+      },
+      {
+        url: "image/img-7.gif",
+        title: "桃花靥",
+        price: "￥18"
+      },
+      {
+        url: "image/img-8.gif",
+        title: "桃夭锦瑟",
+        price: "￥18"
+      },
+      {
+        url: "image/img-9.gif",
+        title: "红袖添香",
+        price: "￥18"
+      },
+      {
+        url: "image/img-10.gif",
+        title: "梅开二度",
+        price: "￥16"
+      },
+      {
+        url: "image/img-11.gif",
+        title: "曼珠沙华",
+        price: "￥15"
+      },
+      {
+        url: "image/img-12.gif",
+        title: "烟波拿铁",
+        price: "￥16"
+      },
+      {
+        url: "image/img-13.gif",
+        title: "一骑红尘",
+        price: "￥16"
+      },
+      {
+        url: "image/img-14.gif",
+        title: "凤栖绿桂",
+        price: "￥18"
+      },
+      {
+        url: "image/img-15.gif",
+        title: "阿里山青",
+        price: "￥10"
+      },
+      {
+        url: "image/img-16.gif",
+        title: "六安遣香",
+        price: "￥10"
+      },
+      {
+        url: "image/img-17.gif",
+        title: "叹红菱",
+        price: "￥10"
+      },
+      {
+        url: "image/img-18.gif",
+        title: "璇玑茗茶",
+        price: "￥10"
+      },
+      {
+        url: "image/img-19.gif",
+        title: "茉黛玉",
+        price: "￥10"
+      }
+    ]
+  },
+  onLoad: function () {
+  },
+  handleShopTap: function() {
+    
+  }
+})  
